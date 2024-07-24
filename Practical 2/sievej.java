4ce27faff90a42b759fd6a00845d8b96a24223ab
Wakanaka Gurure <G22G8069@ict.ru.ac.za>
2024-07-24 19:06:20 +0200

    Adding Task 8 file

---
+++ Practical 2/sievej.java
@@ -0,0 +1,53 @@
+
+// Group Name: Gears of Miracles 
+//
+
+import library.*;
+
+
+class sievej {
+
+    public static void main(String[] args) {
+
+        final int Max = 49000;
+        IntSet primeNums = new IntSet();
+        int n, iterations, primes = 0;
+
+        { IO.write("How many iterations? "); iterations = IO.readInt(); }
+        { IO.write("Supply largest number to be tested "); n = IO.readInt(); }
+
+        if (n > Max) {
+            { IO.write("n too large, sorry"); }
+            return;
+        }
+
+        int it = 1;
+        while (it <= iterations) {
+            primes = 0;
+            primeNums.clear();
+            
+            // Creating a set from 2 to n 
+            for (int i = 2; i <= n; i++) {
+                primeNums.incl(i);
+            }
+
+            /
+            for (int i = 2; i <= n; i++) {
+                
+                if (primeNums.contains(i)) {
+                    primes++;
+                    int k = i + i;
+                // Remove all the numbers that are factors of 2
+                    while (k <= n) {
+                        primeNums.excl(k);
+                        k += i;
+                    }
+                }
+            }
+
+            it++;
+        }
+        {IO.write(primeNums.toString() + "\n");}
+        { IO.write(primes); IO.write(" primes"); }
+    }
+}
